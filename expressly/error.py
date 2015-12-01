d7538abdfc2b68760bf7047eea5316da3749b0fd
Sam Pratt <samj.pratt@gmail.com>
2015-12-01 18:54:11 +0000

    initial commit

---
+++ expressly/error.py
@@ -0,0 +1,26 @@
+class ExpresslyError(Exception):
+    def __init__(self,
+                 message=None,
+                 http_body=None,
+                 http_status=None,
+                 json_body=None,
+                 headers=None):
+        super(ExpresslyError, self).__init__(message)
+
+        self.message = message
+        self.http_body = http_body
+        self.http_status = http_status
+        self.json_body = json_body
+        self.headers = headers or {}
+
+
+class GenericError(ExpresslyError):
+    pass
+
+
+class AuthenticationError(ExpresslyError):
+    pass
+
+
+class UuidError(ExpresslyError):
+    pass
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='expressly',
+    packages=['expressly'],
+    version='0.1',
+    description='Expressly Python 3 SDK',
+    author='Expressly',
+    author_email='info@buyexpressly.com',
+    url='https://github.com/expressly/python-sdk',
+    download_url='',  # latest github tag
+    keywords=[],
+    classifiers=[]
+)
