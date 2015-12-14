3604ac2858fb3c252428f4d3ed5cff41bb29ebd3
Sam Pratt <samj.pratt@gmail.com>
2015-12-14 13:22:06 +0000

    increment version to 2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='expressly',
     packages=['expressly'],
-    version='1.0.0',
+    version='2.0.0',
     description='Expressly Python 3 SDK',
     author='Expressly',
     author_email='info@buyexpressly.com',
